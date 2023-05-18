e9ac55424f04488e135162740943999884b03c90
Wasiu Bakare <wasiubakare@gmail.com>
2023-05-18 18:49:40 +0100

    Setup eslint and prettier config

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,7 +5,6 @@ const tours = JSON.parse(
 );
 
 exports.checkID = (req, res, next, val) => {
-  console.log(`Tour id: ${val}`);
   if (req.params.id * 1 > tours.length)
     return res.status(404).json({
       status: 'fail',
@@ -24,7 +23,6 @@ exports.checkBody = (req, res, next) => {
 };
 
 exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
@@ -49,12 +47,12 @@ exports.getTour = (req, res) => {
 
 exports.createtour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
+  const newTour = { id: newId, ...req.body };
 
   tours.push(newTour);
 
   fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
+    `${__dirname}/../dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
     (err) => {
       if (!err) {
