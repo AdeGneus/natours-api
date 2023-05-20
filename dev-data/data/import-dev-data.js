04e71891707fa4284163cd3e18fbe7aa7a6bb47d
Wasiu Bakare <wasiubakare@gmail.com>
2023-05-20 10:00:55 +0100

    Loaded dev data into database

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE_LOCAL;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB Connection successful!'));
+
+// READ JSON file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// Import data
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data loaded');
+  } catch (error) {
+    console.error(error);
+  }
+  process.exit();
+};
+
+// Delete all data from DB
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data deleted');
+  } catch (error) {
+    console.error(error);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
