aa9853d8ab26bfcd73b517234ad3d70f21317b82
Wasiu Bakare <wasiubakare@gmail.com>
2023-05-22 11:57:22 +0100

    Implemented better error handling

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const mongoose = require('mongoose');
-// eslint-disable-next-line import/no-extraneous-dependencies
 const slugify = require('slugify');
+//const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -17,6 +18,7 @@ const tourSchema = new mongoose.Schema(
         10,
         'A tour name must have more than or equal 10 characters ',
       ],
+      // validate: [validator.isAlpha, 'Tour name must only contain character'],
     },
     slug: String,
     duration: {
@@ -30,10 +32,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium or difficult',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
@@ -43,7 +51,16 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price'],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          // the 'this' keyword only points to current doc on NEW document creation (doesn't work on UPDATE)
+          return val < this.price;
+        },
+        message: 'Discount price ({VALUE}) should be below regular price',
+      },
+    },
     summary: {
       type: String,
       trim: true,
